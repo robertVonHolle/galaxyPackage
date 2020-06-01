e3647bc9ef1a3a620bca03f80b4c66d2733afd39
Robert Von Holle <robert.j.vonholle@gmail.com>
2020-06-01 11:54:57 -0400

    Modularized color vs magnitude plotting

---
+++ galaxy/densityplot.py
@@ -0,0 +1,55 @@
+from galaxy import galaxy
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import time
+
+def densityPlot(galaxies, f, xlim=None, ylim=None):
+	r"""
+	Creates a 2d histogram of galaxies with color on the x-axis and absolute magnitude
+	on the y-axis
+
+	Parameters
+		galaxies - Type: dict. A dictionary of galaxy objects to be plotted
+		f - Type: str. The name of the plot's output file
+		xlim (optional) - Type: list. The bounds of the x axis
+		ylim (optional) - Type: list. The bounds of the y axis
+	
+	Returns nothing but prints the plot to the specified output file.
+	"""
+	errFlag = -9999  # SDSS u-band error flag
+	galaxyList = list(galaxies.values())
+	color = []
+	Mr = []
+
+	for galaxy in galaxyList:
+		if galaxy.u > errFlag:
+			color.append(galaxy.color)
+			Mr.append(galaxy.Mr)
+
+	plt.hist2d(color, Mr, bins=250, norm=LogNorm(), cmin=1, cmap=plt.cm.inferno)
+	plt.gca().invert_yaxis()
+	plt.xlabel("u - r")
+	plt.ylabel("Absolute Magnitude")
+
+	# Implement plot limits if requested by user
+	if xlim != None:
+		if type(xlim) == list:
+			if len(xlim) == 2:
+				plt.xlim(min(xlim),max(xlim))
+			else:
+				raise ValueError("Argument 'xlim' must be a list of length 2. Got list of length %s" % (len(xlim)))
+		else:
+			raise TypeError("Argument 'xlim' must be a list of length 2. Got: %s" % (type(xlim)))
+
+	if ylim != None:
+		if type(ylim) == list:
+			if len(ylim) == 2:
+				plt.ylim(max(ylim),min(ylim))
+			else:
+				raise ValueError("Argument 'ylim' must be a list of length 2. Got list of length %s" % (len(ylim)))
+		else:
+			raise TypeError("Argument 'ylim' must be a list of length 2. Got: %s" % (type(ylim)))
+	
+	plt.colorbar()
+	plt.savefig(f)
+	print("Created galaxy plot")
--- sampleAnalysis2.py
+++ sampleAnalysis2.py
@@ -1,48 +1,15 @@
 from galaxy import galaxy
 from galaxy.datagen import dataGen
 from galaxy.agncount import agnCount
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
+from galaxy.densityplot import densityPlot
 
 # Grab the data to create our dictionary
 galaxies = dataGen('galaxyData/galaxy_sample_2.csv')
-sampleGalaxy = 1237645879578460271
+#sampleGalaxy = 1237645879578460271
 print("Generated 'galaxies' dictionary")
 
-# Debug checks. Feel free to comment/uncomment these as necessary
-#print(galaxies[sampleGalaxy].objId)
-#print(galaxies[sampleGalaxy].ra)
-#print(galaxies[sampleGalaxy].dec)
-#print(galaxies[sampleGalaxy].z)
-#print(galaxies[sampleGalaxy].redshift)
-#print(galaxies[sampleGalaxy].u)
-#print(galaxies[sampleGalaxy].r)
-#print(galaxies[sampleGalaxy].Mr)
-#print(galaxies[sampleGalaxy].color)
-#print(galaxies[sampleGalaxy].agn)
-
+# Count each type and total number of AGNs
 counts = agnCount(galaxies, printCounts=True)
 
-# We're only interested in galaxies with color >0. Figure out the others later
-galaxyList = list(galaxies.values())
-color = []
-Mr = []
-
-elem = 0
-for galaxy in galaxyList:
-	if galaxy.color >= 0:
-		color.append(galaxy.color)
-		Mr.append(galaxy.Mr)
-#	else:
-#		print("Removing object:", galaxy.objId)
-#		elem += 1
-#print("Removed %s objects" % (elem))
-
-# Plot the remaining galaxies to make sure we're looking at the right shape
-plt.hist2d(color, Mr, bins=250, norm=LogNorm(), cmin=1, cmap=plt.cm.inferno)
-plt.gca().invert_yaxis()
-plt.xlim(0.5,4)
-plt.ylim(-18,-24)
-plt.colorbar()
-plt.savefig('galaxy_sample_2_hist2d.png')
-print("Created galaxy plot")
+# Plot the galaxies to make sure we're looking at the right shape
+densityPlot(galaxies, 'galaxy_sample_2_nolim_hist2d.png')
